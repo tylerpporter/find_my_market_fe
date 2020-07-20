5c7cb86810ecf4935b8e265434a14863e15a206e
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-19 18:17:29 -0600

    Move App component under components folder and update entry

---
+++ components/App/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from '../Home/Home'
+
+export default function App() {
+  return (
+    <View>
+      <Home />
+    </View>
+  );
+}
