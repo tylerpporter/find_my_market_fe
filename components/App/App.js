b6b42d06943cd32b5919f82f595398a28c55d388
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-19 18:19:04 -0600

    Update App component to arrow function

--- components/App/App.js
+++ components/App/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from '../Home/Home'
 
-export default function App() {
+export default App = () => {
   return (
     <View>
       <Home />
--- components/SearchNav/SearchNav.js
+++ components/SearchNav/SearchNav.js
@@ -1,58 +1,99 @@
 import React from "react";
 import { Text, View, TextInput, Button, Alert, StyleSheet } from "react-native";
 import { useForm, Controller } from "react-hook-form";
+import Constants from "expo-constants";
 
 const SearchNav = () => {
   const { control, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const cityInputRef = React.useRef();
+  const stateInputRef = React.useRef();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  console.log('errors', errors);
 
   return (
-    <View>
-      <Controller
-        control={control}
-        render={({ onChange, onBlur, value }) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-          />
-        )}
-        name="firstName"
-        rules={{ required: true }}
-        defaultValue=""
-      />
-      {errors.firstName && <Text>This is required.</Text>}
-
-      <Controller
-        control={control}
-        render={({ onChange, onBlur, value }) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-          />
-        )}
-        name="lastName"
-        defaultValue=""
-      />
-
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.label}>City</Text>
+        <Controller
+          name="city"
+          control={control}
+          rules={{ required: "This is required"}}
+          onFocus={() => {
+            cityInputRef.current.focus();
+          }}
+          render={(props) => (
+            <TextInput
+              {...props}
+              style={styles.input}
+              onChangeText={(value) => {
+                props.onChange(value);
+              }}
+              ref={cityInputRef}
+            />
+          )}
+        />
+      </View>
+      {errors.firstName && <Text style={styles.label}>This is required.</Text>}
+      <View>
+        <Text style={styles.label}>State</Text>
+        <Controller
+          name="state"
+          control={control}
+          rules={{ required: "This is required"}}
+          onFocus={() => {
+            stateInputRef.current.focus();
+          }}
+          render={(props) => (
+            <TextInput
+              {...props}
+              style={styles.input}
+              onChangeText={(value) => {
+                props.onChange(value);
+              }}
+              ref={stateInputRef}
+            />
+          )}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button color="white" title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    borderColor: 'black',
-    height: "15%",
-    width: "100%",
+  label: {
+    color: "white",
+    margin: 10,
+    marginHorizontal: 10,
+  },
+  button: {
+    marginTop: 40,
+    color: "white",
+    backgroundColor: "#ec5990",
+    height: 40,
     borderRadius: 4,
-    color: 'black',
-    padding: (10, 15),
-    marginBottom: 10,
-    fontSize: 14,
+  },
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+    backgroundColor: "#0e101c",
+  },
+  input: {
+    backgroundColor: "white",
+    height: 40,
+    width: 130,
+    padding: 10,
+    borderRadius: 4,
+    marginHorizontal: 10,
   },
 });
 
