1a4733ff0b30686d31120404e4e6e0d83e904849
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-19 18:16:43 -0600

    Add MainNav component

---
+++ components/MainNav/MainNav.js
@@ -0,0 +1,31 @@
+import React, { Component, useState } from "react";
+import { FontAwesome, Feather } from "@expo/vector-icons";
+import { Platform, StyleSheet, Text, View, Image, Alert } from "react-native";
+
+const MainNav = () => {
+  return (
+    <View style={styles.container}>
+      <FontAwesome name="filter" size={50} color="black" />
+      <Text style={styles.title}>Find My Market</Text>
+      <Feather name="menu" size={50} color="black" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "blue",
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: "15%",
+    margin: 0,
+    padding: 5,
+  },
+  title: {
+    fontSize: 30,
+  }
+});
+
+export default MainNav;
