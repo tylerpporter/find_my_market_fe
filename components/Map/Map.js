65df53c8052d60ed7f8be85ed4929bc5922f8fbe
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-22 14:54:59 -0600

    Map working where user can see current location and farmers markets 50 miles from their current location

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
-import { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Marker, Callout } from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+} from "react-native";
 import * as Location from "expo-location";
 
 const Map = () => {
-  
-
   const initialRegion = {
     coords: {
-      // latitude: 39.7452,
-      // longitude: -104.9922,
-      // latitudeDelta: 0.5,
-      // longitudeDelta: 0.5,
       latitude: 37.78825,
       longitude: -122.4324,
       latitudeDelta: 0.0922,
@@ -31,85 +30,67 @@ const Map = () => {
         setErrorMsg("Permission to access location was denied");
       }
       let location = await Location.getCurrentPositionAsync({});
+
       setLocation(location);
-      console.log('location', location)
-      // getMarketsNearby()
-      // console.log('location', location)
+      getMarketsNearby(location);
     })();
   }, []);
 
-  useEffect(() => {
-    getMarketsNearby(location)
-  }, [])
-
-  // console.log('latitude', location.coordinates.latitude)
-
   const getMarketsNearby = (location) => {
-
-    let url = "https://us-farmers-markets-api.herokuapp.com/"
+    let url = "https://us-farmers-markets-api.herokuapp.com/";
 
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      
-      body: JSON.stringify({ query: `{ marketsByLocation(lat: ${location.coords.latitude}, lng: ${location.coords.longitude}, radius: 50) { marketname distance latitude longitude } }` }),
-    })
-    .then(response => response.json())
-    .then(data => {
-      setMarketsNearMe(data.data.marketsByLocation)
-      
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    })
-  }
 
-  // console.log('POOOP:', marketsNearMe);
-
-  const handleRegionChange = (region) => {
-    console.log('region', region)
-    let newLocation = {coords: region}
-    if (newLocation != location) {
-      getMarketsNearby(newLocation)
-    }
-    setLocation({coords: region});
-    // console.log('new location', location)
+      body: JSON.stringify({
+        query: `{ marketsByLocation(lat: ${location.coords.latitude}, lng: ${location.coords.longitude}, radius: 50) { marketname distance latitude longitude website season1date season1time street city state zip products { name } } }`,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMarketsNearMe(data.data.marketsByLocation);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   const markers = marketsNearMe.map((location) => {
     let { latitude, longitude } = location;
 
+    let productsList = location.products.map((product) => {
+      return <Text key={Math.random()}>{product.name}</Text>;
+    });
+
     return (
       <Marker
         key={Math.random()}
         coordinate={{ latitude: latitude, longitude: longitude }}
-        // image={require("../../assets/images/tent-location-icon.png")}
-        // onSelect={(e) => matchCampsiteData(e)}
-      />
+      >
+        <Callout>
+          <Text>{location.marketname}</Text>
+          <Text>
+            Address: {location.street} {location.city}, {location.state}{" "}
+            {location.zip}
+          </Text>
+          <Text>Distance Away: {Math.round(location.distance)}</Text>
+          <Text>Website: {location.website}</Text>
+          {productsList}
+        </Callout>
+      </Marker>
     );
   });
 
   return (
-    // <View testID="map-container" style={styles.container}>
-      <MapView
-        // onMarkerDeselect={() => setSelectedCampsite(null)}
-        style={{ height: '55%' }}
-        // provider={PROVIDER_GOOGLE}
-        region={location.coords}
-        showsScale={true}
-        zoomEnabled={true}
-        zoomControlEnabled={true}
-        showsUserLocation={true}
-        followsUserLocation={true}
-        onRegionChangeComplete={(region) => handleRegionChange(region)}
-      >
-        {markers}
-      </MapView>
-      // {/* {selectedCampsite && <QuickView campsite={selectedCampsite} />} */}
-      // {errorMsg && <Text>{errorMsg}</Text>}
-    // {/* // </View> */}
+    <MapView 
+      style={{ height: "68%" }} 
+      showsUserLocation={true}
+    >
+      {markers}
+    </MapView>
   );
 };
 
