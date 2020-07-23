420efc21ce4208fc172b16a7153635eb16260214
Quinn Elder <qre.turing@gmail.com>
2020-07-23 17:08:42 -0600

    Refactor small changes and add TouchableOpacity on lines 35-37

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -7,11 +7,9 @@ import {
   View,
   Linking,
 } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const Map = ({ marketsNearMe, location, searchedMarkets, searchedCity }) => {
-  
-  const [hasSearched, setHasSearched] = useState(false)
-
+const Map = ({ marketsNearMe, location, searchedMarkets, searchedCity, region }) => {
 
   const markers = marketsNearMe.map((location) => {
     let { latitude, longitude, id } = location;
@@ -25,6 +23,7 @@ const Map = ({ marketsNearMe, location, searchedMarkets, searchedCity }) => {
         key={id}
         id={id}
         coordinate={{ latitude: latitude, longitude: longitude }}
+        pinColor='#F25C54'
       >
         <Callout>
           <Text>{location.marketname}</Text>
@@ -33,7 +32,9 @@ const Map = ({ marketsNearMe, location, searchedMarkets, searchedCity }) => {
             {location.zip}
           </Text>
           <Text>Distance Away: {Math.round(location.distance)}</Text>
-          <Text>Website: {location.website}</Text>
+          <TouchableOpacity onPress={()=>{openUrl(location.website)}}>
+            <Text>{location.website}</Text>
+          </TouchableOpacity>
           {productsList}
         </Callout>
       </Marker>
@@ -52,6 +53,7 @@ const Map = ({ marketsNearMe, location, searchedMarkets, searchedCity }) => {
         key={id}
         id={id}
         coordinate={{ latitude: latitude, longitude: longitude }}
+        pinColor='#F4845F'
       >
         <Callout>
           <Text>{location.marketname}</Text>
