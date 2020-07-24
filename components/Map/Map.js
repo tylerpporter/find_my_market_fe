11dc177d37da7cff45638b3be3c27233bf6f60ed
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-24 17:23:59 -0600

    Update map to show region of searched location and just display pins of your location

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -5,80 +5,74 @@ import {
   StyleSheet,
   Text,
   View,
+  Button,
+  TouchableOpacity,
   Linking,
 } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { ScrollView } from "react-native-gesture-handler";
+// import { TouchableOpacity, TouchableHighlight } from "react-native-gesture-handler";
+// import * as Linking from 'expo-linking'
 
-const Map = ({ marketsNearMe, location, searchedMarkets, searchedCity, region }) => {
+const Map = ({
+  marketsNearMe,
+  location,
+  searchedMarkets,
+  searchedCity,
+  region,
+}) => {
 
   const markers = marketsNearMe.map((location) => {
     let { latitude, longitude, id } = location;
 
-    let productsList = location.products.map((product) => {
-      return <Text key={Math.random()}>{product.name}</Text>;
-    });
+    // let productsList = location.products.map((product) => {
+    //   return <Text key={Math.random()}>{product.name}</Text>;
+    // });
 
     return (
       <Marker
         key={id}
         id={id}
         coordinate={{ latitude: latitude, longitude: longitude }}
-        pinColor='#F25C54'
+        pinColor="#F25C54"
       >
-        <Callout>
-          <Text>{location.marketname}</Text>
-          <Text>
-            Address: {location.street} {location.city}, {location.state}{" "}
-            {location.zip}
-          </Text>
-          <Text>Distance Away: {Math.round(location.distance)}</Text>
-          <TouchableOpacity onPress={()=>{openUrl(location.website)}}>
-            <Text>{location.website}</Text>
-          </TouchableOpacity>
-          {productsList}
-        </Callout>
-      </Marker>
-    );
-  });
-
-  const searchedMarkers = searchedMarkets.map((location) => {
-    let { latitude, longitude, id } = location;
-
-    let productsList = location.products.map((product) => {
-      return <Text key={Math.random()}>{product.name}</Text>;
-    });
-
-    return (
-      <Marker
-        key={id}
-        id={id}
-        coordinate={{ latitude: latitude, longitude: longitude }}
-        pinColor='#F4845F'
-      >
-        <Callout>
-          <Text>{location.marketname}</Text>
-          <Text>
-            Address: {location.street} {location.city}, {location.state}{" "}
-            {location.zip}
-          </Text>
-          <Text>Distance Away: {Math.round(location.distance)}</Text>
-          <Text>Website: {location.website}</Text>
-          {productsList}
+        <Callout style={{ width: 300, height: 300 }}>
+          <View style={{margin: 5}}><Text>{location.marketname}</Text></View>
+          <View style={{margin: 5}}><Text style={{ flexWrap: "wrap" }}>
+            {location.street} {location.city}, {location.state} {location.zip}
+          </Text></View>
+          <View style={{margin: 5}}><Text>Distance Away: {Math.round(location.distance)} mile(s)</Text></View>
+          <View style={{margin: 5}}><Text>Season Date: {location.season1date}</Text></View>
+          <View style={{margin: 5}}><Text style={{ flexWrap: "wrap" }}>
+            Season Time: {location.season1time}
+          </Text></View>
+          <View style={{margin: 5}}><Text>Website: {location.website}</Text></View>
+            {/* {productsList} */}
         </Callout>
       </Marker>
     );
   });
 
   return (
-    <MapView 
-      style={{ height: "68%" }} 
+    <MapView
+      style={{ height: "68%" }}
       showsUserLocation={true}
       region={location.coords}
     >
       {markers}
-      {searchedMarkets && searchedMarkers}
     </MapView>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 40,
+    color: "#05668D",
+    backgroundColor: "#F7B267",
+    height: 40,
+    borderRadius: 4,
+    paddingRight: 50,
+    paddingLeft: 50,
+  },
+});
 
 export default Map;
