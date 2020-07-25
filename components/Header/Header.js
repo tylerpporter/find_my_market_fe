f00b51b1b95888e5eb3dabf1e38e384ff4bc6777
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-24 21:47:16 -0600

    Add modal and filtering functionality to header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,28 +1,108 @@
-import React from "react";
-import { StyleSheet, Text, View, Modal, Alert, TouchableHighlight } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  Alert,
+  TouchableHighlight,
+  Switch,
+  FlatList,
+} from "react-native";
 import SearchNav from "../SearchNav/SearchNav";
 import { FontAwesome, Feather } from "@expo/vector-icons";
+import ListItem from "../ListItem/ListItem";
 
-const Header = ({ currentLocation, setModalVisible, modalVisible, setSearchedCity, setSearchedMarkets }) => {
+
+const Header = ({
+  setModalVisible,
+  modalVisible,
+  setSearchedMarkets,
+  setLocation,
+  setMarketsNearMe,
+  getMarketsNearby,
+  location,
+  filteredProducts,
+  setFilteredProducts
+}) => {
+
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      title: "organic",
+      value: false
+    },
+    {
+      id: 2,
+      title: "flowers",
+      value: false
+    },
+    {
+      id: 3,
+      title: "meat",
+      value: false
+    },
+    {
+      id: 4,
+      title: "poultry",
+      value: false
+    },
+    {
+      id: 5,
+      title: "vegetables",
+      value: false
+    },
+    {
+      id: 6,
+      title: "cheese",
+      value: false
+    },
+    {
+      id: 7,
+      title: "honey",
+      value: false
+    },
+    {
+      id: 8,
+      title: "jams",
+      value: false
+    },
+    {
+      id: 9,
+      title: "juices",
+      value: false
+    },
+    {
+      id: 10,
+      title: "coffee",
+      value: false
+    },
+  ]);
+
+
+  console.log('filteredproducts', filteredProducts)
 
   return (
     <View style={styles.container}>
       {/* <MainNav /> */}
       <View style={styles.mainNavContainer}>
-        <TouchableHighlight onPress={() => {setModalVisible(true)}}>
+        <TouchableHighlight
+          onPress={() => {
+            setModalVisible(true);
+          }}
+        >
           <FontAwesome name="filter" size={50} color="black" />
         </TouchableHighlight>
         <Text style={styles.title}>Find My Market</Text>
         <Feather name="menu" size={50} color="black" />
       </View>
 
-      <Text style={styles.location}>{currentLocation}</Text>
-
-      <SearchNav 
-        setSearchedCity={setSearchedCity}
+      <SearchNav
         setSearchedMarkets={setSearchedMarkets}
-        />
-
+        setLocation={setLocation}
+        setMarketsNearMe={setMarketsNearMe}
+        filteredProducts={filteredProducts}
+      />
 
       {/* MODAL WILL LIVE HERE */}
       <Modal
@@ -37,13 +117,22 @@ const Header = ({ currentLocation, setModalVisible, modalVisible, setSearchedCit
           <View style={styles.modalView}>
             <Text style={styles.modalText}>Hello World!</Text>
 
+            <View style={styles.modalContainer}>
+              <FlatList
+                data={products}
+                renderItem={({ item }) => <ListItem  products={products} setFilteredProducts={setFilteredProducts} filteredProducts={filteredProducts} item={item} />}
+                keyExtractor={(item) => item.id}
+              />
+            </View>
+
             <TouchableHighlight
               style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
               onPress={() => {
+                getMarketsNearby(location)
                 setModalVisible(!modalVisible);
               }}
             >
-              <Text style={styles.textStyle}>Hide Modal</Text>
+              <Text style={styles.textStyle}>Filter By Products</Text>
             </TouchableHighlight>
           </View>
         </View>
@@ -54,17 +143,21 @@ const Header = ({ currentLocation, setModalVisible, modalVisible, setSearchedCit
 
 const styles = StyleSheet.create({
   mainNavContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: (0, 20)
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: (0, 20),
   },
   title: {
     fontSize: 30,
   },
   container: {
     height: "30%",
-    marginTop: 50,
+    marginTop: 20,
+  },
+  modalContainer: {
+    height: "60%",
+    marginTop: 20,
   },
   location: {
     flexDirection: "column",
@@ -76,7 +169,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
     margin: 20,
@@ -87,27 +180,27 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
   openButton: {
     backgroundColor: "#F194FF",
     borderRadius: 20,
     padding: 10,
-    elevation: 2
+    elevation: 2,
   },
   textStyle: {
     color: "white",
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
   },
   modalText: {
     marginBottom: 15,
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 });
 
 export default Header;
