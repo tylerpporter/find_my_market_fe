6ec96365e97ceb4d59d53792f7c56215029b453d
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-27 14:03:01 -0600

    Add Login test to test for the user filling out the email input

--- components/Login/Login.test.js
+++ components/Login/Login.test.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { render, waitFor, fireEvent } from "react-native-testing-library";
-import { Button, Text, TextInput, View } from 'react-native'
-import Login from './Login';
+import { act } from "react-test-renderer";
+import Login from "./Login";
 
 describe("Login Page", () => {
+  test("renders a login page upon load", async () => {
+    const { getByText, getByTestId } = render(<Login />);
 
-    test("renders a login page upon load", async () => {
-        const { getByText, getByRole } = render(<Login />);
+    const email = await waitFor(() => getByText("Email:"));
+    const button = await waitFor(() => getByText("Log in"));
+    const logo = await waitFor(() => getByTestId("logo"));
 
-        const email = await waitFor(() => getByText("Email:"))
-        const button = await waitFor(() => getByText("Log in"))
-        // const logo = await waitFor(() => getByRole("image"))
+    expect(email).toBeTruthy();
+    expect(button).toBeTruthy();
+    expect(logo).toBeTruthy();
+  });
 
-        expect(email).toBeTruthy();
-        expect(button).toBeTruthy();
-        // expect(logo).toBeTruthy();
-    })
-})
+  test("user can fill out the email input", async () => {
+    const { getByPlaceholder } = render(<Login />);
+
+    const emailInput = getByPlaceholder("Please enter your email");
+
+    act(() => {
+      fireEvent.changeText(emailInput, "michelle@gmail.com");
+    });
+
+    expect(await waitFor(() => emailInput.props.value)).toEqual("michelle@gmail.com");
+  });
+});
