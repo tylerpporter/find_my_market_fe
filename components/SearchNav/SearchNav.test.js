9caff046da2d0a7a8dcacf5a7898046a5364dbc0
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-27 12:56:23 -0600

    Add city and state form test to SearchNav

--- components/SearchNav/SearchNav.test.js
+++ components/SearchNav/SearchNav.test.js
@@ -1,51 +1,10 @@
 import React from "react";
 import { render, waitFor, fireEvent } from "react-native-testing-library";
-import { Button, Text, TextInput, View } from "react-native";
 import SearchNav from "./SearchNav";
 import { searchNavOnSubmit } from "../../apiCalls";
 import { act } from "react-test-renderer";
 
 jest.mock("../../apiCalls");
-
-const mockData = {
-  marketsByCity: {
-    latitude: 32.7762719,
-    longitude: -96.7968559,
-    markets: [
-      {
-        city: "Dallas",
-        distance: 0.44487885632345103,
-        fmid: 1012749,
-        latitude: 32.777649,
-        longitude: -96.789375,
-        marketname: "Dallas Farmers Market",
-        products: [
-          {
-            name: "organic",
-          },
-          {
-            name: "bakedgoods",
-          },
-          {
-            name: "cheese",
-          },
-          {
-            name: "crafts",
-          },
-        ],
-        season1date: "01/01/2016 to ",
-        season1time: "Fri: 10:00 AM-5:00 PM;Sat: 8:00 AM-5:00 PM;Sun: 10:00 AM-5:00 PM;",
-        state: "Texas",
-        street: "920 S. Harwood Rd.",
-        website: "http://www.dallasfarmersmarket.org",
-        zip: "75201",
-      },
-    ],
-  },
-};
-
-
-
 
 describe("SearchNav", () => {
   test("renders a SearchNav component upon logging in", async () => {
@@ -60,25 +19,37 @@ describe("SearchNav", () => {
     expect(submit).toBeTruthy();
   });
 
-  test("user can fill out the form in the SearchNav to search for a new city and state", () => {
-    searchNavOnSubmit.mockResolvedValue(mockData);
-    const { getByPlaceholder, getByText, queryByTestId, getByTestId } = render(<SearchNav />);
+  test("user can fill out the city and state input", async () => {
+    const { getByPlaceholder } = render(<SearchNav />);
 
-    // const cityInput = getByPlaceholder("Enter a city");
-    // const stateInput = getByPlaceholder("Enter a state");
+    const cityInput = getByPlaceholder("Enter a city");
+    const stateInput = getByPlaceholder("Enter a state");
+
+    act(() => {
+      fireEvent.changeText(cityInput, "Dallas");
+      fireEvent.changeText(stateInput, "Texas");
+    });
+
+    expect(await waitFor(() => cityInput.props.value)).toEqual("Dallas");
+    expect(await waitFor(() => stateInput.props.value)).toEqual("Texas");
+  });
+
+  test("user can fill out the form in the SearchNav to search for a new city and state", async () => {
+    const { getByText, getByPlaceholder } = render(<SearchNav />);
+
+    const cityInput = getByPlaceholder("Enter a city");
+    const stateInput = getByPlaceholder("Enter a state");
     const submit = getByText("Submit");
 
     act(() => {
-      fireEvent(getByPlaceholder("Enter a city"), 'onChange', {nativeEvent: {text: 'Dallas'}})
-      fireEvent(getByPlaceholder("Enter a state"), 'onSubmit')
-      fireEvent(getByText("Submit"), 'onChange', {nativeEvent: {text: 'Dallas'}})
-      // fireEvent.changeText(cityInput, "Dallas");
-      // fireEvent.changeText(stateInput, "Texas");
-      // fireEvent.press(submit);
-    })
+      fireEvent.changeText(cityInput, "Dallas");
+      fireEvent.changeText(stateInput, "Texas");
+    });
 
-    expect(searchNavOnSubmit).toBeCalled();
+    act(() => {
+      fireEvent.press(submit);
+    });
+
+    expect(await waitFor(() => searchNavOnSubmit)).toBeCalled();
   });
 });
-
-
