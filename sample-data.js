e2822035f001be16d323eb36788f8f12c824edf9
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-27 19:32:20 -0600

    Add sample data for map markers test

---
+++ sample-data.js
@@ -0,0 +1,89 @@
+export const data = [
+  {
+    city: "Arvada",
+    distance: 1.1223106533651834,
+    fmid: 1007652,
+    latitude: 39.7897,
+    longitude: -105.1,
+    marketname: "Market at Echters Garden Center",
+    products: [
+      {
+        name: "organic",
+      },
+      {
+        name: "bakedgoods",
+      },
+    ],
+    season1date: "05/06/2017 to 10/28/2017",
+    season1time: "Sat: 9:00 AM-2:00 PM;",
+    state: "Colorado",
+    street: " 5150 Garrison St",
+    website: "http://www.arvadafarmersmarket.com/",
+    zip: 80005,
+  },
+  {
+    city: "Wheat Ridge ",
+    distance: 1.4236942507687225,
+    fmid: 1017973,
+    latitude: 39.788406,
+    longitude: -105.10669,
+    marketname: "Crossroads Artisan And Farmers Community Market",
+    products: [
+      {
+        name: "organic",
+      },
+      {
+        name: "bakedgoods",
+      },
+      {
+        name: "cheese",
+      },
+      {
+        name: "crafts",
+      },
+      {
+        name: "flowers",
+      },
+    ],
+    season1date: "05/06/2017 to 10/28/2017",
+    season1time: "Sat: 9:00 AM-2:00 PM;",
+    state: "Colorado",
+    street: "9725 W. 50th Ave.",
+    website: null,
+    zip: "80033",
+  },
+  {
+    city: "Wheat Ridge",
+    distance: 2.3640541089260636,
+    fmid: 1011779,
+    latitude: 39.769905,
+    longitude: -105.07525,
+    marketname: "Four Seasons Farmers and Artisans Market",
+    products: [
+      {
+        name: "bakedgoods",
+      },
+      {
+        name: "cheese",
+      },
+      {
+        name: "crafts",
+      },
+      {
+        name: "flowers",
+      },
+      {
+        name: "eggs",
+      },
+      {
+        name: "herbs",
+      },
+    ],
+    season1date: "01/01/2016 to 12/31/2016",
+    season1time: "Tue: 12:00 PM-6:00 PM;Wed: 12:00 PM-6:00 PM;Thu: 12:00 PM-6:00 PM;Fri: 12:00 PM-6:00 PM;Sat: 9:00 AM-3:00 PM;Sun: 10:00 AM-2:00 PM;",
+    state: "Colorado",
+    street: "7043 W. 38th Ave.",
+    website: "http://www.fourseasonsfam.com/wordpress/",
+    zip: "80033",
+  },
+];
