15204ed4711ef260ad4adbbb5e103405c6d35ad7
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-07-27 18:07:38 -0600

    Add header tests

--- components/Header/Header.test.js
+++ components/Header/Header.test.js
@@ -1,15 +1,105 @@
 import React from "react";
 import { render, waitFor, fireEvent } from "react-native-testing-library";
-import { Button, Text, TextInput, View } from 'react-native'
-import Header from './Header';
+import Header from "./Header";
+import { getMarketsNearby } from "../../apiCalls";
+import { act } from "react-test-renderer";
+
+jest.mock("../../apiCalls");
 
 describe("Header", () => {
+  test("renders a header upon load", async () => {
+    const { getByTestId } = render(<Header />);
 
-    test("renders a header upon load", () => {
-        const { getByText } = render(<Header />);
+    const title = getByTestId("bannerLogo");
+    const filterBtn = await waitFor(() => getByTestId("filterBtn"));
 
-        const title = getByText("Find My Market")
+    expect(title).toBeTruthy();
+    expect(filterBtn).toBeTruthy();
+  });
 
-        expect(title).toBeTruthy();
-    })
-})
+  test("renders a SearchNav upon load", () => {
+    const { getByText } = render(<Header />);
+
+    const city = getByText("City");
+    const state = getByText("State");
+
+    expect(city).toBeTruthy();
+    expect(state).toBeTruthy();
+  });
+
+  test("a modal appears upon clicking the filter button", () => {
+    const mockModalVisible = jest.fn();
+
+    const { getByTestId, getByText } = render(
+      <Header setModalVisible={mockModalVisible} />
+    );
+
+    const filterBtn = getByTestId("filterBtn");
+
+    act(() => {
+      fireEvent.press(filterBtn);
+    });
+
+    const modalHeader = getByText("Filter By Products");
+
+    expect(modalHeader).toBeTruthy();
+  });
+
+  test("when a user clicks the submit filter button, the appropriate fetch call is made", async () => {
+    const mockModalVisible = jest.fn();
+    const mockSetFilteredProducts = jest.fn();
+
+    const { getByTestId, getByText } = render(
+      <Header
+        setModalVisible={mockModalVisible}
+        setFilteredProducts={mockSetFilteredProducts}
+      />
+    );
+
+    const filterBtn = getByTestId("filterBtn");
+
+    act(() => {
+      fireEvent.press(filterBtn);
+    });
+
+    const submitBtn = getByText("Submit Filter");
+
+    expect(submitBtn).toBeTruthy();
+
+    act(() => {
+      fireEvent.press(submitBtn);
+    });
+
+    expect(await waitFor(() => getMarketsNearby)).toBeCalled();
+  });
+
+  test("when a user clicks the clear current filters button, the appropriate fetch call is made", async () => {
+    const mockModalVisible = jest.fn();
+    const mockSetFilteredProducts = jest.fn();
+    const mockSetProducts = jest.fn();
+
+    const { getByTestId, getByText } = render(
+      <Header
+        setModalVisible={mockModalVisible}
+        setFilteredProducts={mockSetFilteredProducts}
+        setProducts={mockSetProducts}
+      />
+    );
+
+    const filterBtn = getByTestId("filterBtn");
+
+    act(() => {
+      fireEvent.press(filterBtn);
+    });
+
+    const clearfiltersBtn = getByText("Clear Current Filters");
+
+    expect(clearfiltersBtn).toBeTruthy();
+
+    act(() => {
+      fireEvent.press(clearfiltersBtn);
+    });
+
+    expect(await waitFor(() => getMarketsNearby)).toBeCalled();
+  });
+});
